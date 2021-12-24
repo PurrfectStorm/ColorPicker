f50da2e40e45c2b7b7be0ddba74077c2a972c4b2
Holy <aggronewbie@gmail.com>
2021-12-24 16:01:18 +0300

    Fix crashes due to too large tap recognition area

--- ColorPicker/ColorPickerVC.swift
+++ ColorPicker/ColorPickerVC.swift
@@ -14,11 +14,11 @@ class ColorPickerVC: UIViewController {
     var colorDescription: UILabel!
     
     override func loadView() {
+        
         view = UIView()
         mainImage = UIImageView()
-        //mainImage.image = UIImage(named: "test.jpg")
         mainImage.translatesAutoresizingMaskIntoConstraints = false
-        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector (pointTapped(_:))))
+        mainImage.isUserInteractionEnabled = true
         view.addSubview(mainImage)
         
         colorPreview = UIView(frame: CGRect(x: view.center.x + 80 , y: view.center.y + 700 , width: 50, height: 50))
@@ -51,15 +51,12 @@ class ColorPickerVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        mainImage.setNeedsDisplay()
-        // Do any additional setup after loading the view.
+        mainImage.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector (pointTapped(_:))))
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         let url = Bundle.main.url(forResource: "colors", withExtension: "png")!
-        //        let scaleFactor = UIScreen.main.scale
-        //        let scale = CGAffineTransform(scaleX: scaleFactor, y: scaleFactor)
         let image = resizedImage(at: url, for: mainImage.bounds.size)
         self.mainImage.image = image
     }
@@ -85,30 +82,19 @@ class ColorPickerVC: UIViewController {
         guard let components = color.cgColor.components, components.count >= 3 else {
             return nil
         }
+        
         let r = Float(components[0])
         let g = Float(components[1])
         let b = Float(components[2])
-        ////        var a = Float(1.0)
-        //
-        //        if components.count >= 4 {
-        //            a = Float(components[3])
-        //        }
         
-        //        if alpha {
-        //            return String(format: "%02lX%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255), lroundf(a * 255))
-        //        } else {
         return String(format: "%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255))
-        //        }
-        
+ 
     }
 }
 
 extension UIImage {
     func getPixelColor(pos: CGPoint) -> UIColor {
-        
-        //        let pixelData = self.cgImage!.dataProvider!.data
-        //        let data: UnsafePointer<UInt8> = CFDataGetBytePtr(pixelData)
-        
+
         let size = self.size
         let dataSize = size.width * size.height * 4
         var pixelData = [UInt8](repeating: 0, count: Int(dataSize))
